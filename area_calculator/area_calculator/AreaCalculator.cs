48a9eddb0f41987065479fd2e4e815ee8f5f0b1d
Daniil Serebryakov <52635499+Sonariooo@users.noreply.github.com>
2021-01-28 17:21:43 +0300

    Add files via upload

---
+++ area_calculator/area_calculator/AreaCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_calculator
+{
+    public class AreaCalculator
+    {
+        /// <summary>
+        /// Площадь круга
+        /// </summary>
+        /// <param name="rad"></param>
+        /// <returns></returns>
+        public double GetArea(double rad)
+        {
+            return 3.14 * rad * rad;
+        }
+
+        /// <summary>
+        /// Площадь прямоугольника
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double GetArea(double a, double b)
+        {
+            return a * b;
+        }
+        /// <summary>
+        /// Площадь треугольника
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public double GetArea(double a, double b,double c)
+        {
+            double p = (a + b + c) / 2; //по герону
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        /// <summary>
+        /// Проверка на прямоугольность
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public bool CheckTriangle(double a, double b, double c)
+        {
+            if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b))
+                return true;
+            return false;
+
+        }
+
+    }
+}
---
+++ area_calculator/area_calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("area_calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("area_calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5c41fde2-6565-449b-8fdb-0ca8bf275ad2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
